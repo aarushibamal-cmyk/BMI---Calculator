12b3dbca3bfd26b690bad2bb14acd6a22aa1cdc2
aarushibamal-cmyk <aarushibamal@gmail.com>
2026-06-17 13:03:50 +0530

    feat: create BMI calculator with validation and weight classification

---
+++ script.js
@@ -0,0 +1,31 @@
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const height = parseFloat(document.querySelector('#height').value)
+    const weight = parseFloat(document.querySelector('#weight').value)
+    const result = document.querySelector('#result')
+    if (height == '' || height < 0 || isNaN(height)) {
+        result.innerHTML = "Please give a valid height!"
+    }
+    else if (weight == '' || weight < 0 || isNaN(weight)) {
+        result.innerHTML = "Please give a valid weight"
+    }
+    else{
+        const bmi = (weight / ((height*height)/10000)).toFixed(2)
+        result.innerHTML = `Your BMI is ${bmi}`
+        if(bmi<=18.5){
+            result.innerHTML = `Your BMI is ${bmi} <p>UnderWeight</p>`
+        }
+        if(bmi<=24.9){
+            result.innerHTML = `Your BMI is ${bmi} <p>Normal Weigth</p>`
+        }
+        if(bmi<=29.9){
+            result.innerHTML = `Your BMI is ${bmi} <p>Overweight</p>`
+        }
+        if(bmi>=30){
+            result.innerHTML = `Your BMI is ${bmi} <p>Obese</p>`
+        }
+    }
+});
